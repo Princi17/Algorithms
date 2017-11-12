f002414f552271c806333f51d505d0aae3ef807c
princi <princinarula4@gmail.com>
2017-11-12 10:58:54 +0530

    selected_sort

---
+++ selected_sort.rb
@@ -0,0 +1,16 @@
+def selected_sort(a)
+  n = a.length
+  for i in 0...n
+    min = i
+    for j in (i+1)...n
+      if a[min] > a[j]
+        temp_num = a[j]
+        a[j] = a[min]
+        a[min] = temp_num
+      end
+    end
+  end
+  return a
+end
+
+a = [3,6,2,8,1,5]
